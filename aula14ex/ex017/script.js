556b8801e3f53348d820904a998706ef0c9aaae0
Richard Brasil <82055788+richardbrasil1@users.noreply.github.com>
2022-09-14 15:18:58 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -9,12 +9,19 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c <= 10) {
+        /*while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n * c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
+        }*/
+        /* USANDO FOR*/
+        for (c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
         }
     }
         
